fad73f39b6d4c44f1a854a666b26dea9c0f8f7a4
raza <razakhadim57@gmail.com>
2018-05-09 19:00:52 +0500

    By Raza

---
+++ CustomerFinal/CustomerFinal/cust2R.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomerFinal
+{
+    public partial class cust2R : Form
+    {
+        public cust2R()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            cust1 frm = new cust1();
+            frm.Show();
+            this.Hide();
+        }
+
+        private void bunifuFlatButton1_Click(object sender, EventArgs e)
+        {
+            localhost.Service1 v = new localhost.Service1();
+            bool is_exist = false; ;
+            foreach(localhost.CustClass1  cust in v.showAll())
+            {
+                if(cust.Name== bunifuMetroTextbox1.Text && cust.Password== bunifuMetroTextbox2.Text)
+                {
+                    is_exist = true;
+                }
+            }
+            if (is_exist)
+            {
+                MessageBox.Show("Already exist Try something different ");
+
+            }
+            else
+            {
+                v.isad(bunifuMetroTextbox1.Text, bunifuMetroTextbox2.Text, bunifuMetroTextbox3.Text, bunifuMetroTextbox4.Text);
+                MessageBox.Show("ADDED");
+                bunifuMetroTextbox1.Text = " ";
+                bunifuMetroTextbox2.Text = " ";
+                bunifuMetroTextbox3.Text = " ";
+                bunifuMetroTextbox4.Text = " ";
+            }
+        }
+    }
+}
---
+++ custwcf/custwcf/CustClass1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Linq;
+using System.Web;
+
+namespace custwcf
+{
+
+    [DataContract]
+    public class CustClass1
+    {
+        private string name;
+        private string password;
+        private string npassword;
+        private string email;
+        private string ip;
+        [DataMember]
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+        [DataMember]
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+        [DataMember]
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+        [DataMember]
+        public string Ip
+        {
+            get
+            {
+                return ip;
+            }
+
+            set
+            {
+                ip = value;
+            }
+        }
+        [DataMember]
+        public string Npassword
+        {
+            get
+            {
+                return npassword;
+            }
+
+            set
+            {
+                npassword = value;
+            }
+        }
+    }
+}
