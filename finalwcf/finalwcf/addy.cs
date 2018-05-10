c080d5e2a88e07e7f18a2882bd967ef7f6ac57f9
Sheikh Sheharyar <ssheharyar1997@gmail.com>
2018-05-11 00:04:05 +0500

    I Have made changes in server for admin task

---
+++ finalwcf/finalwcf/addy.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace finalwcf
+{
+    public class addy
+    {
+        private string service;
+        private string route;
+        private string routee;
+        private string bus;
+        private string capacity;
+
+        public string Service
+        {
+            get
+            {
+                return service;
+            }
+
+            set
+            {
+                service = value;
+            }
+        }
+
+        public string Route
+        {
+            get
+            {
+                return route;
+            }
+
+            set
+            {
+                route = value;
+            }
+        }
+
+        public string Routee
+        {
+            get
+            {
+                return routee;
+            }
+
+            set
+            {
+                routee = value;
+            }
+        }
+
+        public string Bus
+        {
+            get
+            {
+                return bus;
+            }
+
+            set
+            {
+                bus = value;
+            }
+        }
+
+        public string Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+
+            set
+            {
+                capacity = value;
+            }
+        }
+    }
+}
---
+++ finalwcf/finalwcf/addy2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace finalwcf
+{
+    public class addy2
+    {
+        public static List<addy> db = new List<addy>();
+        
+        public static List<string> showe1 = new List<string>();
+        public static List<string> showe2 = new List<string>();
+        public static List<string> showe3 = new List<string>();
+        public static List<string> showe4 = new List<string>();
+        public static List<string> showe5 = new List<string>();
+
+        public void deletpp(int index)
+        {
+            db.RemoveAt(index);
+
+        }
+        public void update(int index, string bus, string capac)
+        {
+            addy a = new addy();
+            a = db.ElementAt(index);
+            a.Bus = bus;
+            a.Capacity = capac;
+
+        }
+
+    }
+}
